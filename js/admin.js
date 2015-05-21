e258bd889e6ab9a78fcc2356f12799699b0ef1a9
Irene Avilés <ireneavibel@gmail.com>
2015-05-21 19:13:50 +0200

    login

--- js/admin.js
+++ js/admin.js
@@ -25,39 +25,75 @@ $(function() {
 
  	});
 */
- 	var LoginView = Parse.View.extend({
-	 		template: Handlebars.compile($('#login-tpl').html()),
-	 		events: {
-	 			'submint .form-signin': 'login'
-	 		},
-	 		login: function(e){
-	 			e.preventDefault();
-	 			var data=$(e.target).serializeArray(),
-	 				username = data[0].value,
-	 				password = data[1].value;
 
-	 			Parse.User.logIn(username,password,{
-	 				success:function(user){
-	 					alert('Welcome!');
-	 				},
-	 				error: function(user, error){
-	 					console.log(error);
-	 				}
-	 			});
-	 		},
-	 		render:function(){
-	 			this.$el.html(this.template());
-	 		}
- 		}),
- 		WelcomeView = Parse.View.extend({
- 			template: Handlebars.compile($('welcome-tpl').html()),
- 			render: function(){
- 				var attributes = this.model.toJSON();
- 				this.$el.html(this.template(attributes));
- 			}
- 	});
+	var WelcomeView = Parse.View.extend({
+        template: Handlebars.compile($('#welcome-tpl').html()),
+        render: function(){
+            var attributes = this.model.toJSON();
+            this.$el.html(this.template(attributes));
+        }
+    });
 
+
+	var LoginView = Parse.View.extend({
+    	template: Handlebars.compile($('#login-tpl').html()),
+    	events: {
+        	'submit .form-signin': 'login'
+    	},
+    	login: function(e) {
+ 
+        	// Prevent Default Submit Event
+        	e.preventDefault();
+ 
+       		// Get data from the form and put them into variables
+	        var data = $(e.target).serializeArray(),
+	            username = data[0].value,
+	            password = data[1].value;
+ 
+	        // Call Parse Login function with those variables
+	        Parse.User.logIn(username, password, {
+	            // If the username and password matches
+	            success: function(user) {
+	                //alert('Welcome!');
+	                var welcomeView = new WelcomeView({ model: user });
+				    welcomeView.render();
+				    $('.main-container').html(welcomeView.el);
+	            },
+	            // If there is an error
+	            error: function(user, error) {
+	                console.log(error);
+	            }
+	        });
+    	},
+	        render: function(){
+	        this.$el.html(this.template());
+	    }
+	})
 	
+	
+
+	var loginView = new LoginView();
+	loginView.render();
+	$('.main-container').html(loginView.el);
+
+
+
+	/*
+	var LoginView = Parse.View.extend({
+        template: Handlebars.compile($('#login-tpl').html()),
+        render: function(){
+            this.$el.html(this.template());
+        }
+    }),
+    WelcomeView = Parse.View.extend({
+        template: Handlebars.compile($('#welcome-tpl').html()),
+        render: function(){
+            var attributes = this.model.toJSON();
+            this.$el.html(this.template(attributes));
+        }
+    });
+
+	*/
 
     /*
     var TestObject = Parse.Object.extend("TestObject");
@@ -65,9 +101,6 @@ $(function() {
     testObject.save({foo: "bar"}).then(function(object) {
       alert("yay! it worked");
     });
-
-	
-
 	*/
  
 });
